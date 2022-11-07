9649351f7bfb1e53a758746a9b438cbceb3c781f
rosegolden1677 <makela.adams@yahoo.com>
2022-11-06 19:35:59 -0800

    add data file

---
+++ src/data.js
@@ -0,0 +1,179 @@
+import React from "react";
+
+export const ballons = [
+  {
+    id: 1,
+    type: 'set',
+    img: 'https://i.postimg.cc/87SkDd8t/black-panther.png',
+    name: 'Black Panther Set',
+    price: 19.99
+  },
+  {
+    id: 2,
+    type: 'individual',
+    img: 'https://i.postimg.cc/bG6qQKZB/blue-dog.png',
+    name: 'Blue Dog',
+    price: 5.99
+  },
+  {
+    id: 3,
+    type: 'set',
+    img: 'https://i.postimg.cc/njgZWBYK/cocomelon-set.png',
+    name: 'Cocomelon Set',
+    price: 19.99
+  },
+  {
+    id: 4,
+    type: 'set',
+    img: 'https://i.postimg.cc/ygksnv9Y/fairy-set.png',
+    name: 'Fairy Set',
+    price: 19.99
+  },
+  {
+    id: 5,
+    type: 'individual',
+    img: 'https://i.postimg.cc/CRshKZSY/frozen.png',
+    name: 'Frozen',
+    price: 5.99
+  },
+  {
+    id: 6,
+    type: 'set',
+    img: 'https://i.postimg.cc/bGYzSzwn/frozen-set.png',
+    name: 'Frozen Set',
+    price: 19.99
+  },
+  {
+    id: 7,
+    type: 'individual',
+    img: 'https://i.postimg.cc/WtqskYT5/hello-kitty.png',
+    name: 'Hello Kitty',
+    price: 5.99
+  },
+  {
+    id: 8,
+    type: 'set',
+    img: 'https://i.postimg.cc/1VFP1XJk/hero-team-set.png',
+    name: 'Hero Team Set',
+    price: 19.99
+  },
+  {
+    id: 9,
+    type: 'individual',
+    img: 'https://i.postimg.cc/4mCsbv3s/mario-party.png',
+    name: 'Mario Party',
+    price: 5.99
+  },
+  {
+    id: 10,
+    type: 'set',
+    img: 'https://i.postimg.cc/FYNvw9c6/mario-set.png',
+    name: 'Mario Set',
+    price: 19.99
+  },
+  {
+    id: 11,
+    type: 'individual',
+    img: 'https://i.postimg.cc/7fgDBCC7/mermaid.png',
+    name: 'Mermaid',
+    price: 5.99
+  },
+  {
+    id: 12,
+    type: 'set',
+    img: 'https://i.postimg.cc/McGSKd9b/mermaid-set.png',
+    name: 'Mermaid Set',
+    price: 19.99
+  },
+  {
+    id: 13,
+    type: 'individual',
+    img: 'https://i.postimg.cc/Hrzmgf9k/mickey-head.png',
+    name: 'Mickey Mouse Head',
+    price: 5.99
+  },
+  {
+    id: 14,
+    type: 'individual',
+    img: 'https://i.postimg.cc/nswtsB58/mickey-mouse.png',
+    name: 'Mickey Mouse',
+    price: 5.99
+  },
+  {
+    id: 15,
+    type: 'individual',
+    img: 'https://i.postimg.cc/Cnby6tKY/minion.png',
+    name: 'Minion',
+    price: 5.99
+  },
+  {
+    id: 16,
+    type: 'individual',
+    img: 'https://i.postimg.cc/BLr9ps4M/peppa-pig.png',
+    name: 'Peppa Pig',
+    price: 5.99
+  },
+  {
+    id: 17,
+    type: 'set',
+    img: 'https://i.postimg.cc/1nn1D4h8/pokemon-set.png',
+    name: 'Pokemon Set',
+    price: 19.99
+  },
+  {
+    id: 18,
+    type: 'set',
+    img: 'https://i.postimg.cc/z3h1BLFM/princess-animals.png',
+    name: 'Princess and Friends Set',
+    price: 19.99
+  },
+  {
+    id: 19,
+    type: 'set',
+    img: 'https://i.postimg.cc/3dHQDKLw/princess-set.png',
+    name: 'Princess Set',
+    price: 19.99
+  },
+  {
+    id: 20,
+    type: 'set',
+    img: 'https://i.postimg.cc/dh4bXGJ7/shark-set.png',
+    name: 'Shark Set',
+    price: 19.99
+  },
+  {
+    id: 21,
+    type: 'individual',
+    img: 'https://i.postimg.cc/cvxqXFGy/sonic.png',
+    name: 'Sonic',
+    price: 5.99
+  },
+  {
+    id: 22,
+    type: 'individual',
+    img: 'https://i.postimg.cc/HV4DwfK9/spiderman.png',
+    name: 'Spiderman',
+    price: 19.99
+  },
+  {
+    id: 23,
+    type: 'set',
+    img: 'https://i.postimg.cc/sBxkR12T/spongebob.png',
+    name: 'Spongebob Set',
+    price: 19.99
+  },
+  {
+    id: 24,
+    type: 'set',
+    img: 'https://i.postimg.cc/gX61n58R/transformers-set.png',
+    name: 'Transformer Set',
+    price: 19.99
+  },
+  {
+    id: 25,
+    type: 'set',
+    img: 'https://i.postimg.cc/zLQ9D5b8/unicorn-set.png',
+    name: 'Unicorn Set',
+    price: 19.99
+  },
+]
