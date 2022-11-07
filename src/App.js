360507c68f9579aa6a32b3106b50252f4962298b
rosegolden1677 <makela.adams@yahoo.com>
2022-11-06 20:41:43 -0800

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import Home from './pages/Home';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Balloons from './pages/Balloons';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" index element={<Home />} />
+        <Route path="/balloons" index element={<Balloons />} />
+        <Route path="/cart" index element={<Cart />} />
       </Routes>
     </Router>
   );
