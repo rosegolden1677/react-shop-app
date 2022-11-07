e57e262de0a18c0341a3fc0aa9da8d65a124245a
rosegolden1677 <makela.adams@yahoo.com>
2022-11-06 18:15:43 -0800

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Home from './pages/Home';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" index element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
